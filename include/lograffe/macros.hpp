5ad9d1978495d09eade6028009c718d5650aa022
Ingmar Runge <ingmar@irsoft.de>
2017-04-17 14:23:49 +0200

    master: macros for user-managed logger instances

--- include/lograffe/macros.hpp
+++ include/lograffe/macros.hpp
@@ -8,8 +8,17 @@
 #pragma once
 
 #define LOG(LEVEL) \
-	if (lograffe::logger::current()->level_enabled(lograffe::log_level::LEVEL)) \
-		lograffe::detail::log_entry(lograffe::logger::current(), lograffe::log_level::LEVEL, #LEVEL)
+	if (lograffe::logger::current().level_enabled(lograffe::log_level::LEVEL)) \
+		lograffe::log_entry(lograffe::logger::current(), lograffe::log_level::LEVEL, #LEVEL)
 
 #define LOG_IF(BOOLEAN_EXPR, LEVEL) \
 	if (BOOLEAN_EXPR) LOG(LEVEL)
+
+// same as LOG, but with custom logger. LOGGER must be an instance of lograffe::logger.
+#define LOGR(LOGGER, LEVEL) \
+	if (LOGGER.level_enabled(lograffe::log_level::LEVEL)) \
+		lograffe::log_entry(LOGGER, lograffe::log_level::LEVEL, #LEVEL)
+
+// same as LOG_IF, but with custom logger.
+#define LOGR_IF(BOOLEAN_EXPR, LOGGER, LEVEL) \
+	if (BOOLEAN_EXPR) LOGR(LOGGER, LEVEL)
