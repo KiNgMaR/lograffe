d550e80e76f8e02270c3661097d28ab9ca5beba6
Ingmar Runge <ingmar@irsoft.de>
2017-04-17 14:23:03 +0200

    master: allow detaching sinks

--- include/lograffe/detail/logger_impl.hpp
+++ include/lograffe/detail/logger_impl.hpp
@@ -17,12 +17,14 @@ namespace lograffe
 		return static_cast<uint_fast32_t>(level) >= enabled_levels_;
 	}
 
-	inline void logger::attach_sink(std::unique_ptr<sink>&& new_sink)
+	inline logger::attached_sink_handle logger::attach_sink(std::unique_ptr<sink>&& new_sink)
 	{
 		if (!new_sink)
 		{
-			return;
+			return attached_sink_handle();
 		}
+
+		attached_sink_handle new_sink_handle = std::hash<void*>()(new_sink.get());
 
 		sinks_.emplace_back(std::move(new_sink));
 
@@ -34,6 +36,20 @@ namespace lograffe
 		}
 
 		enabled_levels_ = new_levels;
+
+		return new_sink_handle;
+	}
+
+	inline void logger::detach_sink(attached_sink_handle handle)
+	{
+		// TODO: this is ugly
+
+		const auto comparison = [handle](const std::unique_ptr<sink>& element) -> bool
+			{ return std::hash<void*>()(element.get()) == handle; };
+
+		sinks_.erase(
+			std::remove_if(sinks_.begin(), sinks_.end(), comparison),
+			sinks_.end());
 	}
 
 	inline void logger::log(const log_entry& entry) noexcept
--- include/lograffe/logger.hpp
+++ include/lograffe/logger.hpp
@@ -11,32 +11,47 @@
 #include <lograffe/log_entry.hpp>
 #include <lograffe/sink.hpp>
 #include <lograffe/formatter.hpp>
+#include <lograffe/detail/thread_local_static_instance.hpp>
 #include <vector>
+#include <functional>
 
 namespace lograffe
 {
 
-	class logger
+	class logger : public detail::thread_local_static_instance<logger>
 	{
 	public:
+		// default constructed = no-op logger
 		logger()
 			: enabled_levels_()
 		{}
 
-		bool level_enabled(log_level level) const;
-		void log(const log_entry& entry) noexcept;
+		// disallow copy construction and copy assignment
+		logger(const logger&) = delete;
+		logger(logger&&) = default;
+		logger& operator=(const logger&) = delete;
+		logger& operator=(logger&&) = default;
 
-		void attach_sink(std::unique_ptr<sink>&& new_sink);
+	public:
+		typedef std::hash<void*>::result_type attached_sink_handle;
+
+		attached_sink_handle attach_sink(std::unique_ptr<sink>&& new_sink);
 
 		// to allow inline construction of sinks:
 		template<class TSink, class TFormatter, class... TSinkArgs>
-		void attach_sink(log_level min_level, TSinkArgs&&... args)
+		attached_sink_handle attach_sink(log_level min_level, TSinkArgs&&... args)
 		{
 			static_assert(std::is_base_of<sink, TSink>::value, "expecting type derived from lograffe::sink");
 			static_assert(std::is_base_of<formatter, TFormatter>::value, "expecting type derived from lograffe::formatter");
 
-			attach_sink(std::make_unique<TSink>(min_level, std::make_unique<TFormatter>(), std::forward<TSinkArgs>(args)...));
+			return attach_sink(std::make_unique<TSink>(min_level, std::make_unique<TFormatter>(), std::forward<TSinkArgs>(args)...));
 		}
+
+		void detach_sink(attached_sink_handle handle);
+
+	public:
+		bool level_enabled(log_level level) const;
+		void log(const log_entry& entry) noexcept;
 
 	private:
 		uint_fast32_t enabled_levels_; // contains a bitmask of all levels that are enabled on at least one sink
