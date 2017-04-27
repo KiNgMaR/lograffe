b735bd78867ad9d3b4156af31d12ec34ad962ab2
Ingmar Runge <ingmar@irsoft.de>
2017-04-27 19:27:04 +0200

    master: less indention

--- include/lograffe/detail/thread_local_static_instance.hpp
+++ include/lograffe/detail/thread_local_static_instance.hpp
@@ -10,39 +10,39 @@
 namespace lograffe
 {
 
-	namespace detail
+namespace detail
+{
+
+	// This is a helper class to facilitate a static thread local singleton
+	// of type T within a header-only library. T must be default-constructible.
+	template<typename T>
+	class thread_local_static_instance
 	{
-
-		// This is a helper class to facilitate a static thread local singleton
-		// of type T within a header-only library. T must be default-constructible.
-		template<typename T>
-		class thread_local_static_instance
+	public:
+		static T& current() noexcept
 		{
-		public:
-			static T& current() noexcept
-			{
-				return current_thread_instance_;
-			}
+			return current_thread_instance_;
+		}
 
-			static void set_current(T&& new_instance) noexcept
-			{
-				T old_instance = std::move(current_thread_instance_);
-				current_thread_instance_ = std::move(new_instance);
-				new_instance = std::move(old_instance);
-			}
+		static void set_current(T&& new_instance) noexcept
+		{
+			T old_instance = std::move(current_thread_instance_);
+			current_thread_instance_ = std::move(new_instance);
+			new_instance = std::move(old_instance);
+		}
 
-			static void reset_current() noexcept
-			{
-				current_thread_instance_ = T();
-			}
+		static void reset_current() noexcept
+		{
+			current_thread_instance_ = T();
+		}
 
-		private:
-			static thread_local T current_thread_instance_;
-		};
+	private:
+		static thread_local T current_thread_instance_;
+	};
 
-		template<typename T>
-		thread_local T thread_local_static_instance<T>::current_thread_instance_;
+	template<typename T>
+	thread_local T thread_local_static_instance<T>::current_thread_instance_;
 
-	}
+}
 
 }
--- include/lograffe/detail/time_formatter.hpp
+++ include/lograffe/detail/time_formatter.hpp
@@ -15,75 +15,75 @@
 namespace lograffe
 {
 
-	namespace detail
+namespace detail
+{
+
+	class time_formatter
 	{
-
-		class time_formatter
+	public:
+		static std::string to_rfc3339(const std::chrono::system_clock::time_point& system_timestamp)
 		{
-		public:
-			static std::string to_rfc3339(const std::chrono::system_clock::time_point& system_timestamp)
-			{
-				std::time_t unix_timestamp{ std::chrono::system_clock::to_time_t(system_timestamp) };
+			std::time_t unix_timestamp{ std::chrono::system_clock::to_time_t(system_timestamp) };
 
-				unsigned int millis = std::chrono::duration_cast<std::chrono::milliseconds>(
-					system_timestamp.time_since_epoch()).count() % 1000;
+			unsigned int millis = std::chrono::duration_cast<std::chrono::milliseconds>(
+				system_timestamp.time_since_epoch()).count() % 1000;
 
-				std::stringstream ss;
+			std::stringstream ss;
 
 #ifdef _WIN32
-				std::tm tm;
-				gmtime_s(&tm, &unix_timestamp);
+			std::tm tm;
+			gmtime_s(&tm, &unix_timestamp);
 #else
-				std::tm tm;
-				localtime_r(&unix_timestamp, &tm);
+			std::tm tm;
+			localtime_r(&unix_timestamp, &tm);
 #endif
 
-				ss << (tm.tm_year + 1900) << '-';
-				pad2(ss, tm.tm_mon + 1) << '-';
-				pad2(ss, tm.tm_mday);
-				ss << 'T';
-				pad2(ss, tm.tm_hour) << ':';
-				pad2(ss, tm.tm_min) << ':';
-				pad2(ss, tm.tm_sec) << '.';
-				pad3(ss, millis);
+			ss << (tm.tm_year + 1900) << '-';
+			pad2(ss, tm.tm_mon + 1) << '-';
+			pad2(ss, tm.tm_mday);
+			ss << 'T';
+			pad2(ss, tm.tm_hour) << ':';
+			pad2(ss, tm.tm_min) << ':';
+			pad2(ss, tm.tm_sec) << '.';
+			pad3(ss, millis);
 
 #ifdef _WIN32
-				ss << 'Z';
+			ss << 'Z';
 #else
-				ss << (tm.__tm_gmtoff < 0 ? '-' : '+');
-				// TODO: this is super unportable
-				pad2(ss, tm.__tm_gmtoff / 3600) << ':';
-				pad2(ss, tm.__tm_gmtoff % 3600);
+			ss << (tm.__tm_gmtoff < 0 ? '-' : '+');
+			// TODO: this is super unportable
+			pad2(ss, tm.__tm_gmtoff / 3600) << ':';
+			pad2(ss, tm.__tm_gmtoff % 3600);
 #endif
-				return ss.str();
-			}
+			return ss.str();
+		}
 
-			/*static std::string to_syslog(const std::chrono::system_clock::time_point& timestamp)
-			{
+		/*static std::string to_syslog(const std::chrono::system_clock::time_point& timestamp)
+		{
 
-			}*/
+		}*/
 
-		private:
-			static std::stringstream& pad2(std::stringstream& ss, unsigned int n)
-			{
-				if (n < 10)
-					ss << '0';
-				ss << n;
-				return ss;
-			}
+	private:
+		static std::stringstream& pad2(std::stringstream& ss, unsigned int n)
+		{
+			if (n < 10)
+				ss << '0';
+			ss << n;
+			return ss;
+		}
 
-			static std::stringstream& pad3(std::stringstream& ss, unsigned int n)
-			{
-				if (n < 10)
-					ss << '0';
-				if (n < 100)
-					ss << '0';
-				ss << n;
-				return ss;
-			}
+		static std::stringstream& pad3(std::stringstream& ss, unsigned int n)
+		{
+			if (n < 10)
+				ss << '0';
+			if (n < 100)
+				ss << '0';
+			ss << n;
+			return ss;
+		}
 
-		};
+	};
 
-	}
+}
 
 }
